8d9b64bb5d7401f84578e9a26ada8a085e4a0447
alex0033 <chanchanmen0807@icloud.com>
2021-01-10 01:58:45 +0900

    finish root_class test

--- src/test/suite/models/root.test.ts
+++ src/test/suite/models/root.test.ts
@@ -1,83 +1,84 @@
-// import * as assert from 'assert';
-// import { beforeEach, afterEach, describe, it } from 'mocha';
-// import { ProgectData } from '../../../models/project';
-// import { Root } from '../../../models/root';
-// import { PROGECT_DATA_PATH } from '../../../tools/const';
+import * as assert from 'assert';
+import { beforeEach, afterEach, describe, it } from 'mocha';
+import { ProgectData } from '../../../models/project';
+import { Root, RootData } from '../../../models/root';
 
-// // requireでいい？？importで統一？？
-// const mockfs = require('mock-fs');
+// requireでいい？？importで統一？？
+const mockfs = require('mock-fs');
 
-// const existPath = "/root/exist"
-// const existRootData = {
-//     rootPath: existPath,
-//     files: []
-// }
-// const testData = [existRootData]
-// const testDataString = JSON.stringify(testData, null, "\t")
+const existRootPath = "/root/exist"
+const existRootData: RootData = {
+    rootPath: existRootPath,
+    files: []
+}
+const testData = [existRootData]
+const testDataString = JSON.stringify(testData, null, "\t")
 
-// describe('Root Test Suite', () => {
-//     beforeEach(() => {
-//         // この書き方はスマートではない
-//         // "data"ってやだな！！
-//         // 入っている関数使えるのではdirectoryごと作るてきな！
-//         // リファクタリング対象として進めましょう
-//         mockfs({
-//             data: {
-//                 "progect_data.json": testDataString
-//             }
-//         });
-//     });
+describe('Root Test Suite', () => {
+    beforeEach(() => {
+        // この書き方はスマートではない
+        // "data"ってやだな！！
+        // 入っている関数使えるのではdirectoryごと作るてきな！
+        // リファクタリング対象として進めましょう
+        mockfs({
+            data: {
+                "progect_data.json": testDataString
+            }
+        });
+        ProgectData.loadData();
+    });
 
-//     afterEach(() => {
-//         mockfs.restore();
-//     });
+    afterEach(() => {
+        mockfs.restore();
+    });
 
-//     it('check mock', () => {
-//         // ProgectData.egetData();
-//         assert.ok(ProgectData.fetchRootData(existPath) === undefined, "It should save data");
-//         // assert.ok(ProgectData.fetchRootData(existPath) !== undefined, "It should save data");
-//         // console.log(ProgectData.fetchRootData(existPath));
-//         // ProgectData.egetData();
-//     }); 
+    // CREATE
+    // データの保存確認はProgectDataクラステストの役割！！
+	it('cretate newRoot', () => {
+        const rootPath = "/root/new"
+        const root = Root.create(rootPath);
+        assert.strictEqual(root?.rootPath, rootPath);
 
-// 	it('cretate new root', () => {
-//         ProgectData.egetData();
-//         const rootPath = "root/new"
-//         const root = Root.create(rootPath);
-//         ProgectData.egetData();
-//         if (root) {
-//             // インスタンスが適切に作られているか
-//             assert.ok(root.rootPath === rootPath, "It should make instance");
-//             // データが保存されているか
-//             assert.ok(ProgectData.fetchRootData(rootPath) !== undefined, "It should save data");
-//         } else {
-//             assert.ok(false, "It should not show this message. It cannot make testEnvironment.");
-//         }
-//     });
+        const rootForConfirm = Root.findBy(rootPath);
+        assert.deepStrictEqual(rootForConfirm?.rootPath, rootPath);
+    });
 
-//     // it('cretate exist root again', () => {
-//     //     const root = Root.create(existPath);
-//     //     assert.ok(root === undefined, "It should not make instance");
-//     // });
+    it('cannot cretate existRoot', () => {
+        const root = Root.create(existRootPath);
+        assert.strictEqual(root, undefined);
+    });
 
-//     // it('find exist root', () => {
-//     //     const root = Root.create(existPath);
-        
-//     //     if (root) {
-//     //         // インスタンスが適切に作られているか
-//     //         assert.ok(root.rootPath === existPath, "It should make instance");
-//     //         // データの存在確認
-//     //         assert.ok(ProgectData.fetchRootData(existPath) !== undefined, "It should save data");
-//     //     } else {
-//     //         assert.ok(false, "It should not show this message. It cannot make testEnvironment.");
-//     //     }
-//     // });
+    // READ
+    it('find existRoot', () => {
+        const root = Root.findBy(existRootPath);
+        assert.strictEqual(root?.rootPath, existRootPath);
+    });
 
-//     // it('updatate root', () => {
-        
-//     // });
+    it('cannot find notExistRoot', () => {
+        const root = Root.findBy("/not/exist/path");
+        assert.strictEqual(root, undefined);
+    });
 
-//     // it('delete root', () => {
-    
-//     // });
-// });
+    // UPDATE
+    it('updatate exisRoot', () => {
+        const updatePath = "/root/update/path";
+        let root = Root.findBy(existRootPath);
+        root?.update(updatePath);
+        assert.strictEqual(root?.rootPath, updatePath);
+
+        const previousRoot = Root.findBy(existRootPath);
+        const nextRoot = Root.findBy(updatePath);
+        assert.strictEqual(previousRoot, undefined);
+        assert.notStrictEqual(nextRoot, undefined);
+    });
+
+    // DELETE
+    it('delete root', () => {
+        let root = Root.findBy(existRootPath);
+        root?.destroy();
+        const DestroyedRoot = Root.findBy(existRootPath);
+        // 本当はこの仕様にしたい！！
+        // assert.strictEqual(root, undefined);
+        assert.strictEqual(DestroyedRoot, undefined);
+    });
+});
